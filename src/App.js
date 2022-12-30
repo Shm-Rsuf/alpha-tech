533860f8ad6963ea3d3bad0fa3ddf8700705dd32
shm_usuf <shmusuf@gmail.com>
2022-12-30 08:23:39 +0600

    router setup added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="app min-h-screen bg-gray-50 text-gray-700">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
